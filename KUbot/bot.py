3b2f33b8819ba3fe3630ab02c9a9080a250a7ea6
Phoorinut Hochuei <kokret04@gmail.com>
2023-11-11 10:34:57 +0700

    Add rps command

--- KUbot/bot.py
+++ KUbot/bot.py
@@ -9,10 +9,6 @@ import pymyku
 from utils import *
 import random
 
-#Load bot token
-load_dotenv()
-TOKEN = os.getenv("API_TOKEN")
-
 #Colors for each day of the week
 DAY_COLORS = {
     'MON': Colour.yellow(),
@@ -23,13 +19,17 @@ DAY_COLORS = {
     'SAT': Colour.purple(),
     'SUN': Colour.red()
 }
+api_timeout_day = 15
+user_data = {} #User data container
 
-#Set up the Discord bot
+#Set up Discord bot
 intents = discord.Intents.all()
 intents.message_content = True  
-
 client = commands.Bot(command_prefix='!', intents=intents)
-user_data = {} #User data container
+#Load bot token
+load_dotenv()
+TOKEN = os.getenv("API_TOKEN")
+
 
 @client.event
 async def on_ready():
@@ -81,6 +81,7 @@ async def register(ctx):
         username, password = response_msg.content.split(':') 
         ku_client = pymyku.Client(username, password)
         course = ku_client.fetch_group_course() 
+        #course = {"code": "success", "results": [{"peroid_date": "26/06/2566-20/10/2566", "course": [{"section_id": 229512, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01999111-64", "subject_name_th": "ศาสตร์แห่งแผ่นดิน", "subject_name_en": "Knowledge of the Land", "section_code": "804", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "นัฎฐวิกา จันทร์ศรี,จิรเกียรติ ทรายทอง", "teacher_name_en": "Chirakiat Saithong,Natthavika Chansri", "day_w_c": "1000000", "time_from": "8:00", "time_to": "10:00", "day_w": "MON ", "room_name_th": "อาคาร 13 พลศึกษา", "room_name_en": "อาคาร 13 พลศึกษา", "time_start": 480}, {"section_id": 228965, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01418112-65", "subject_name_th": "แนวคิดการโปรแกรมเบื้องต้น", "subject_name_en": "Fundamental Programming Concepts", "section_code": "800", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "ฉัตรชัย เกษมทวีโชค", "teacher_name_en": "Chatchai Kasemtaweechok", "day_w_c": "0100000", "time_from": "10:00", "time_to": "12:00", "day_w": "TUE ", "room_name_th": "17301", "room_name_en": "17301", "time_start": 600}, {"section_id": 228972, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01418141-65", "subject_name_th": "ทรัพย์สินทางปัญญาและจรรยาบรรณวิชาชีพ", "subject_name_en": "Intellectual Properties and Professional Ethics", "section_code": "800", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "อรวรรณ วัชนุภาพร,เฟื่องฟ้า เป็นศิริ", "teacher_name_en": "Fuangfar Pensiri,Orawan Watchanupaporn", "day_w_c": "0010000", "time_from": "9:00", "time_to": "12:00", "day_w": "WED ", "room_name_th": "17210", "room_name_en": "17210", "time_start": 540}, {"section_id": 228777, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01417111-65", "subject_name_th": "แคลคูลัส I", "subject_name_en": "Calculus I", "section_code": "800", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "จุฬาลักษณ์ แก้วหวังสกูล", "teacher_name_en": "Julalak Kaewwangsakoon", "day_w_c": "0010000", "time_from": "13:00", "time_to": "16:00", "day_w": "WED ", "room_name_th": "1408/1", "room_name_en": "1408/1", "time_start": 780}, {"section_id": 228963, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01418111-65", "subject_name_th": "วิทยาการคอมพิวเตอร์เบื้องต้น", "subject_name_en": "Introduction to Computer Science", "section_code": "800", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "สุชาดา ชมจันทร์", "teacher_name_en": "Suchada Chomjan", "day_w_c": "0010000", "time_from": "16:30", "time_to": "18:30", "day_w": "WED ", "room_name_th": "17201", "room_name_en": "17201", "time_start": 990}, {"section_id": 228966, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01418112-65", "subject_name_th": "แนวคิดการโปรแกรมเบื้องต้น", "subject_name_en": "Fundamental Programming Concepts", "section_code": "830", "section_type": "16902", "section_type_th": "ปฏิบัติ", "section_type_en": "Laboratory", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "ฉัตรชัย เกษมทวีโชค", "teacher_name_en": "Chatchai Kasemtaweechok", "day_w_c": "0001000", "time_from": "8:00", "time_to": "10:00", "day_w": "THU ", "room_name_th": "26512", "room_name_en": "26512", "time_start": 480}, {"section_id": 227180, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01999021-64", "subject_name_th": "ภาษาไทยเพื่อการสื่อสาร", "subject_name_en": "Thai Language for Communication", "section_code": "803", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "โกวิทย์ พิมพวง,อาจารย์พิเศษ .", "teacher_name_en": "Kowit Pimpuang,Visiting Lecturer ", "day_w_c": "0001000", "time_from": "16:30", "time_to": "19:30", "day_w": "THU ", "room_name_th": "17401", "room_name_en": "17401", "time_start": 990}, {"section_id": 233683, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "01355209-64", "subject_name_th": "ภาษาอังกฤษเพื่อการสื่อสารในงานอาชีพ", "subject_name_en": "Communicative English for Careers", "section_code": "802", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "บัณฑิตา สันติกุล", "teacher_name_en": "Bandita Santikul", "day_w_c": "0000100", "time_from": "9:00", "time_to": "12:00", "day_w": "FRI ", "room_name_th": "17207", "room_name_en": "17207", "time_start": 540}, {"section_id": 227268, "groupheader": "26/06/2566-20/10/2566", "weekstartday": "2023-06-26T00:00:00.000Z", "weekendday": "2023-10-20T00:00:00.000Z", "std_id": "246115", "subject_code": "03751112-64", "subject_name_th": "สังคมและการเมือง", "subject_name_en": "Social and Politics", "section_code": "801", "section_type": "16901", "section_type_th": "บรรยาย", "section_type_en": "Lecture", "student_status_code": "17001", "std_status_th": "ปกติ", "std_status_en": "Regular", "teacher_name": "เติมศักดิ์ สุขวิบูลย์", "teacher_name_en": "Toemsak Sukhvibul", "day_w_c": "0000100", "time_from": "13:00", "time_to": "16:00", "day_w": "FRI ", "room_name_th": "17305", "room_name_en": "17305", "time_start": 780},{"section_id":999999,"groupheader":"26/06/2566-20/10/2566","weekstartday":"2023-06-26T00:00:00.000Z","weekendday":"2023-10-20T00:00:00.000Z","std_id":"246115","subject_code":"TEST-123","subject_name_th":"Test","subject_name_en":"Test","section_code":"999","section_type":"99999","section_type_th":"Test","section_type_en":"Test","student_status_code":"17001","std_status_th":"ปกติ","std_status_en":"Regular","teacher_name":"Test Teacher","teacher_name_en":"Test Teacher","day_w_c":"0000010","time_from":"11:30","time_to":"12:30","day_w":"SAT ","room_name_th":"Test Room","room_name_en":"Test Room","time_start":690}]}], "cache": True}
         edu = ku_client.fetch_student_education() 
         api_call_time = time.time() #Get time that api is called
         user_data[user.id] = {
@@ -144,6 +145,7 @@ async def table(ctx):
         await ctx.send("No timetable data available.")
     else:
         await ctx.send(Timetable)
+    print("Table command called")
 
 #Send User education data
 @client.command()
@@ -153,13 +155,38 @@ async def mydata(ctx):
             return
     edu_embed = create_education_embed(user_data[user_id]['Education'])
     await ctx.send(embed=edu_embed)
+    print("Mydata command called")
 
 #Send a random number
 @client.command()
 async def rng(ctx, max : int = 10):
     random_number = random.randint(0, max)
     await ctx.send(random_number)
+    print("Rng command called")
 
+@client.command()
+async def rps(ctx, user_input: int):
+    rps = ["🪨", "📄", "✂️"]
+    await ctx.send(f"You choose:{rps[user_input]}")
+    
+    if user_input < 0 or user_input > 2:
+        print("Please type in 0-2")
+        return
+    com_choice = random.randint(0, 2)
+    await ctx.send(f"Computer choose:{rps[com_choice]}")
+    if user_input == 0 and com_choice == 2:
+        await ctx.send("You win!")
+    elif com_choice == 0 and user_input == 2:
+        await ctx.send("You lose")
+    elif com_choice > user_input:
+        await ctx.send("You lose")
+    elif user_input > com_choice:
+        await ctx.send("You win!")
+    elif com_choice == user_input:
+        await ctx.send("It's a draw")
+
+
+#Check if user is registered or not
 async def user_check(ctx, user_id):
     if user_id not in user_data:
         await ctx.send("You are not registered. Please use the !register command.")
@@ -169,7 +196,7 @@ async def user_check(ctx, user_id):
     last_api_call = user_info.get("last_api_call", 0)
     current_time = time.time()
 
-    if current_time - last_api_call >= 86400:
+    if current_time - last_api_call >= 86400 * api_timeout_day:
         await ctx.send("It's been more than 1 day since the last data update. Please use the !register command again.")
         return 
 
